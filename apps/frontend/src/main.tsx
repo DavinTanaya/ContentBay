14b33090b931f0e93779f8e7d4731e9079ffaf70
DeanFebrio <febriodean@gmail.com>
2026-02-15 20:48:25 +0700

    feat: setup tailwindcss

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="797950478091-l6gvt7p61l6ti7erbmrqs974iaoo7vk7.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<GoogleOAuthProvider clientId='797950478091-l6gvt7p61l6ti7erbmrqs974iaoo7vk7.apps.googleusercontent.com'>
+			<App />
+		</GoogleOAuthProvider>
+	</React.StrictMode>,
 );
--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,31 +1,32 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import GoogleButton from "../auth/GoogleButton";
-import { useAuth } from "../auth/AuthContext";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import GoogleButton from '../auth/GoogleButton';
+import { useAuth } from '../auth/AuthContext';
 
 export default function Login() {
-  const auth = useAuth();
-  const navigate = useNavigate();
+	const auth = useAuth();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (auth.isAuthenticated) {
-      navigate("/", { replace: true });
-    }
-  }, [auth.isAuthenticated, navigate]);
+	useEffect(() => {
+		if (auth.isAuthenticated) {
+			navigate('/', { replace: true });
+		}
+	}, [auth.isAuthenticated, navigate]);
 
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: 20,
-      }}
-    >
-      <h1>Login</h1>
-      <GoogleButton />
-    </div>
-  );
+	return (
+		<div
+			className='bg-amber-100'
+			style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexDirection: 'column',
+				gap: 20,
+			}}
+		>
+			<h1>Login</h1>
+			<GoogleButton />
+		</div>
+	);
 }
