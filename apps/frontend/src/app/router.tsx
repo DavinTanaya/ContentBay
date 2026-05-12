c4b7a127148220735a3c8ac93fe9b65ea64a00b3
cherrypellaa <piecesofchille@gmail.com>
2026-05-12 08:39:37 +0700

    fix: merge conflict

--- apps/frontend/src/app/router.tsx
+++ apps/frontend/src/app/router.tsx
@@ -3,7 +3,18 @@ import { createBrowserRouter } from 'react-router-dom';
 import Login from '@/pages/contentbay/Login';
 import Register from '@/pages/contentbay/Register';
 import Home from '@/pages/contentbay/Home';
-import Protect from '@/shared/components/layout/contentbay/Protect';
+import Product from '@/pages/contentbay/Product';
+import Resources from '@/pages/contentbay/Resources';
+import Documentation from '@/pages/contentbay/Documentation';
+import Layout from '@/shared/components/layout/contentbay/Layout';
+
+import ContentModelList from '@/pages/dashboard/ContentModelList';
+import ContentTypeBuilder from '@/pages/dashboard/ContentTypeBuilder';
+import ContentModelDetail from '@/pages/dashboard/ContentModelDetail';
+import ContentTypeCreate from '@/pages/dashboard/ContentTypeCreate';
+import ContentList from '@/pages/dashboard/ContentList';
+import ContentCreate from '@/pages/dashboard/ContentCreate';
+import DashboardLayout from '@/shared/components/layout/dashboard/DashboardLayout';
 
 export const router = createBrowserRouter([
   {
@@ -16,10 +27,54 @@ export const router = createBrowserRouter([
   },
   {
     path: '/',
-    element: (
-      <Protect>
-        <Home />
-      </Protect>
-    ),
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'product',
+        element: <Product />,
+      },
+      {
+        path: 'resources',
+        element: <Resources />,
+      },
+      {
+        path: 'documentation',
+        element: <Documentation />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: 'content-model',
+        element: <ContentModelList />,
+      },
+      {
+        path: 'content-model/create',
+        element: <ContentTypeCreate />,
+      },
+      {
+        path: 'content-model/:modelId',
+        element: <ContentTypeBuilder />,
+      },
+      {
+        path: 'content-model/:modelId/settings',
+        element: <ContentModelDetail />,
+      },
+      {
+        path: 'content',
+        element: <ContentList />,
+      },
+      {
+        path: 'content/create',
+        element: <ContentCreate />,
+      },
+    ],
   },
 ]);
